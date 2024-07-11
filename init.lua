9d035be40528a9a92027885871d19731073d4335
Daan Mortier <daan@Daans-MacBook-Pro.local>
2024-07-11 15:28:40 +0200

    Changed key mappings for mini.surround to mimic tpope's style
    This way the `s` key remains unaffected

--- init.lua
+++ init.lua
@@ -810,7 +810,17 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        mappings = {
+          add = 'ys',
+          delete = 'ds',
+          find = '',
+          find_left = '',
+          highlight = '',
+          replace = 'cs',
+          update_n_lines = '',
+        },
+      }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
