780ea9b7e22609ed0193b8d7af6a5d3153f5ceb5
Daan Mortier <daan@Daans-MacBook-Pro.local>
2024-07-16 22:51:23 +0200

    Autocomplete: Added <C-e> to abort autocomplete at any time

--- init.lua
+++ init.lua
@@ -780,6 +780,7 @@ require('lazy').setup({
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
           ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<C-e>'] = cmp.mapping.abort(),
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
