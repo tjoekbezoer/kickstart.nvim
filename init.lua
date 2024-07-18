2c8e7cd9eee1b740478cb852a6f577549fd1f8d9
Daan Mortier <daan@Daans-MacBook-Pro.local>
2024-07-19 01:13:42 +0200

    Some quality of life keymaps

--- init.lua
+++ init.lua
@@ -85,6 +85,20 @@ vim.keymap.set('n', 'cd', function()
   vim.fn.chdir(cur_directory)
 end)
 
+-- Cursor repositioning corrections
+vim.keymap.set('n', 'J', 'mzJ`z')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- Delete without replacing yank register
+vim.keymap.set('x', '<leader>p', [["_dP]])
+vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
+-- Yank straight to system clipboard
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
