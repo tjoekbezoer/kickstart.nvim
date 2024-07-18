71e9c8a57c4d4bf25e65e3766a247c46d3285e6d
Daan Mortier <daan@Daans-MacBook-Pro.local>
2024-07-18 13:09:30 +0200

    Added keybindings for moving between tabs

--- init.lua
+++ init.lua
@@ -190,6 +190,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Keybindings for changing tabs
+vim.keymap.set('n', '<C-,>', 'gT', { desc = 'Move to the prev tab' })
+vim.keymap.set('n', '<C-.>', 'gt', { desc = 'Move to the next tab' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
