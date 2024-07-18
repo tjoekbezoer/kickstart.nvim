418e7cbb7131e3973be2942f5b646509a9a6604a
Daan Mortier <daan@Daans-MacBook-Pro.local>
2024-07-19 01:11:06 +0200

    Telescope: Added `<leader>sc` to search colorschemes

--- init.lua
+++ init.lua
@@ -356,6 +356,9 @@ require('lazy').setup({
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
+      vim.keymap.set('n', '<leader>sc', function()
+        builtin.colorscheme { enable_preview = true }
+      end, { desc = 'Search [C]olorschemes' })
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
