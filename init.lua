427499447a3e6de29ea991c128196b5c2e64ef01
Daan Mortier <daan@Daans-MacBook-Pro.local>
2024-07-21 00:35:13 +0200

    Tweaked cursor movement a bit

--- init.lua
+++ init.lua
@@ -113,11 +113,26 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+-- Arrow keys for movement
+vim.keymap.set('n', '<left>', 'h')
+vim.keymap.set('n', '<right>', 'l')
+-- Set vertical arrow keys to visual line movement (this takes
+-- into account line wrapping). When jumping to a relative line,
+-- at it to the jumplist
+vim.keymap.set('n', '<up>', function()
+  if vim.v.count > 1 then
+    return "m'" .. vim.v.count .. 'gk'
+  else
+    return 'gk'
+  end
+end, { expr = true })
+vim.keymap.set('n', '<down>', function()
+  if vim.v.count > 1 then
+    return "m'" .. vim.v.count .. 'gj'
+  else
+    return 'gj'
+  end
+end, { expr = true })
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
