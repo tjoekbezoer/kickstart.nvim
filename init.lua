c8d8a046da25ed14c839e221716ccd659dcfb9c9
Daan Mortier <daan@Daans-MacBook-Pro.local>
2024-07-21 01:21:25 +0200

    Reduced scrolloff

--- init.lua
+++ init.lua
@@ -64,7 +64,7 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 20
+vim.opt.scrolloff = 5
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
