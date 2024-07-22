8b661cdee5d06babf145d75564d801512e232db9
Daan Mortier <daan@Daans-MacBook-Pro.local>
2024-07-22 22:21:10 +0200

    Trying out a new theme: zenbones
    Also added `<leader>tt` to toggle between light/dark mode

--- init.lua
+++ init.lua
@@ -812,21 +812,37 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+  -- {
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   init = function()
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --
+  --     -- You can configure highlights by doing something like:
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
+  {
+    'zenbones-theme/zenbones.nvim',
+    dependencies = { 'rktjmp/lush.nvim' },
 
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+    priority = 1000,
+    init = function()
+      vim.cmd.colorscheme 'zenbones'
+      vim.o.background = 'light'
+    end,
+    -- Toggle between light and dark version of the theme
+    config = function()
+      vim.keymap.set('n', '<leader>tt', function()
+        local is_light = vim.o.background == 'light'
+        if is_light then
+          vim.o.background = 'dark'
+        else
+          vim.o.background = 'light'
+        end
+        vim.o.termguicolors = true
+        vim.cmd.colorscheme(vim.g.colors_name)
+      end)
     end,
   },
 
