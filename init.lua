066f190ea6315a986563891ce54ef53502250b33
Daan Mortier <daan@Daans-MacBook-Pro.local>
2024-07-22 17:40:41 +0200

    Added context plugin
    Sticks relevant lines to the top of the screen, for improved orientation

--- init.lua
+++ init.lua
@@ -972,6 +972,10 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    opts = {},
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
