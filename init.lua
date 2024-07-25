48ccb712a2a96eacfc8cf8dde4cceb75564cf882
Daan Mortier <daan@Daans-MacBook-Pro.local>
2024-07-25 22:19:59 +0200

    Harpoon: changed keybinding for showing list to `<leader>k`

--- init.lua
+++ init.lua
@@ -217,7 +217,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>a', function()
         harpoon:list():add()
       end, { desc = '[A]dd to Harpoon' })
-      vim.keymap.set('n', '<C-e>', function()
+      vim.keymap.set('n', '<leader>k', function()
         harpoon.ui:toggle_quick_menu(harpoon:list())
       end, { desc = 'Open Harpoon window' })
 
