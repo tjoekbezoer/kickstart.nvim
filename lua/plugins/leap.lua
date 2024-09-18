5ab9237207890aac1fde91744004a339163b16d9
Daan Mortier <i@dontwant.email>
2024-09-18 14:37:41 +0200

    Changed keybindings for leap plugin

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -5,16 +5,14 @@ return {
     -- leap.opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
     -- leap.create_default_mappings()
 
-    -- vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
-    -- vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
-    -- vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
+    vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
+    vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+    vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
 
-    vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
-    vim.keymap.set({ 'n', 'x', 'o' }, 'S', function()
-      require('leap.remote').action()
-    end)
-    vim.keymap.set('n', 'gs', '<Plug>(leap-from-window)')
-    -- vim.keymap.set({ 'x', 'o' }, 's', '<Plug>(leap-forward)')
-    -- vim.keymap.set({ 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+    -- vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
+    -- vim.keymap.set({ 'n', 'x', 'o' }, 'S', function()
+    --   require('leap.remote').action()
+    -- end)
+    -- vim.keymap.set('n', 'gs', '<Plug>(leap-from-window)')
   end,
 }
