b797eda03c3b5b9f34ce9b96fe68dc67c45af70e
Daan Mortier <i@dontwant.email>
2024-11-18 22:42:12 +0100

    Added prettier formatter

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -30,7 +30,9 @@ return { -- Autoformat
       --
       -- You can use a sub-list to tell conform to run *until* a formatter
       -- is found.
-      -- javascript = { { "prettierd", "prettier" } },
+      javascript = { 'prettier' },
+      typescript = { 'prettier' },
+      typescriptreact = { 'prettier' },
     },
   },
 }
