692b02ce04b634e0e2643bcf5f031d3b223a530d
Daan Mortier <i@dontwant.email>
2024-11-18 22:42:35 +0100

    Removed not working `tsserver` LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -146,10 +146,6 @@ return {
       --
       -- Some languages (like typescript) have entire language plugins that can be useful:
       --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`tsserver`) will work just fine
-      tsserver = {},
-      --
 
       lua_ls = {
         -- cmd = {...},
