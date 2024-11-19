ac99e3016a064a12d8c9dfd5d3e7bfbf4e937add
Daan Mortier <i@dontwant.email>
2024-11-19 22:41:28 +0100

    Leap: Fix for annoying text dimming upstream
    Somewhere in an upstream commit, the to-be-searched text is dimmed for some reason. This code is an attempt to reverse that behavior. It's not needed now since I reverted the update, but you never know...

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -9,6 +9,14 @@ return {
     vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
     vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
 
+    -- vim.api.nvim_set_hl(0, 'LeapBackdrop', {})
+    -- vim.api.nvim_create_autocmd('OptionSet', {
+    --   pattern = 'background',
+    --   callback = function()
+    --     vim.api.nvim_set_hl(0, 'LeapBackdrop', {})
+    --   end,
+    -- })
+
     -- vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
     -- vim.keymap.set({ 'n', 'x', 'o' }, 'S', function()
     --   require('leap.remote').action()
