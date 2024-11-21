7741aab20dda0a42a15c32dafab3311a118212a8
Daan Mortier <i@dontwant.email>
2024-11-21 14:31:39 +0100

    Added square bracket navigation for git chunks and comments

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,4 +1,4 @@
-return {
+return { -- Adds git related signs to the gutter, as well as utilities for managing changes
   'lewis6991/gitsigns.nvim',
   opts = {
     signs = {
@@ -8,5 +8,15 @@ return {
       topdelete = { text = '‾' },
       changedelete = { text = '~' },
     },
+
+    on_attach = function(buffer)
+      local gs = require 'gitsigns'
+      local function map(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+      end
+
+      map('n', ']g', gs.next_hunk, 'Next Chunk')
+      map('n', '[g', gs.prev_hunk, 'Prev Chunk')
+    end,
   },
 }
--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -3,5 +3,18 @@ return {
   'folke/todo-comments.nvim',
   event = 'VimEnter',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = { signs = false },
+  config = function()
+    local todo = require 'todo-comments'
+    todo.setup {
+      signs = false,
+    }
+
+    vim.keymap.set('n', ']t', function()
+      require('todo-comments').jump_next()
+    end, { desc = 'Next todo comment' })
+
+    vim.keymap.set('n', '[t', function()
+      require('todo-comments').jump_prev()
+    end, { desc = 'Previous todo comment' })
+  end,
 }
