2ce8a584918d637a3c6dea1accfc2dcf726cd33d
Daan Mortier <i@dontwant.email>
2026-05-17 22:36:40 +0200

    Keys: Minor improvements

--- lua/keys.lua
+++ lua/keys.lua
@@ -7,19 +7,29 @@ vim.keymap.set('v', '>', '>gv')
 -- Move text vertical
 vim.keymap.set('n', '<A-j>', '<cmd>m .+1<cr>==', { desc = 'Move Down' })
 vim.keymap.set('n', '<A-k>', '<cmd>m .-2<cr>==', { desc = 'Move Up' })
+vim.keymap.set('n', '<A-down>', '<cmd>m .+1<cr>==', { desc = 'Move Down' })
+vim.keymap.set('n', '<A-up>', '<cmd>m .-2<cr>==', { desc = 'Move Up' })
+
 vim.keymap.set('i', '<A-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move Down' })
 vim.keymap.set('i', '<A-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move Up' })
+vim.keymap.set('i', '<A-down>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move Down' })
+vim.keymap.set('i', '<A-up>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move Up' })
+
 vim.keymap.set('v', '<A-j>', ":m '>+1<cr>gv=gv", { desc = 'Move Down' })
 vim.keymap.set('v', '<A-k>', ":m '<-2<cr>gv=gv", { desc = 'Move Up' })
+vim.keymap.set('v', '<A-down>', ":m '>+1<cr>gv=gv", { desc = 'Move Down' })
+vim.keymap.set('v', '<A-up>', ":m '<-2<cr>gv=gv", { desc = 'Move Up' })
 
 -- Make 'delete to end of paragraph' behave more logical
-vim.keymap.set('n', 'd}', '^d}')
+vim.keymap.set('n', 'd}', 'V}d')
 vim.keymap.set('n', 'd{', 'gj^d{O<esc>gj')
 
 -- Registers
 -- ---------
 -- Delete without replacing yank register
-vim.keymap.set('x', '<leader>p', [["_dP]], { desc = 'Paste without replacing yank register' })
+vim.keymap.set('x', '<leader>p', function()
+  return vim.fn.mode() == 'V' and [["_dP]] or [["_c<C-r>+<Esc>]]
+end, { expr = true })
 vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]], { desc = 'Delete without replacing yank register' })
 
 -- Yank entire file
